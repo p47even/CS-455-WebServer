52a488f0a4d92f143a79f4eb69a83b36d9c96bbf
Eli Arao <earao@hotmail.com>
2021-10-13 20:29:13 -0700

    Changed some information to make it more understandable for the user

--- public_html/showPassengers.php
+++ public_html/showPassengers.php
@@ -5,10 +5,10 @@
 <form action="showPassengers.php" name='form' method='get'>
 Attr: <input type="text" name="attr" id="attr"><br>
 <form action="showPassengers.php" name='form' method='get'>
-INF: <input type="text" name="inf" id="inf"><br>
+Var: <input type="text" name="var" id="var"><br>
 <input type="submit" name="submit" value="Submit">
 
-<h2>List of all passengers</h2>
+<h2>Query Results</h2>
 <p>
     <?php
 
@@ -23,7 +23,7 @@ INF: <input type="text" name="inf" id="inf"><br>
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             //return the html query
-            $query_str = "select * from passengers where $_GET[attr]='$_GET[inf]';"; // <------ Line 19
+            $query_str = "select * from passengers where $_GET[attr]='$_GET[var]';"; // <------ Line 19
             $result_set = $db->query($query_str);
 
             //loop through each tuple in result set and print out the data
