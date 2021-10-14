95012ae7fcb84dbbe285ffd79af952c9081a84ab
Eli Arao <earao@hotmail.com>
2021-10-13 19:43:56 -0700

    Made show passengers.php as of the criteria of part 3

---
+++ public_html/showPassengers.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>List of all passengers</h2>
+<p>
+    <?php
+
+        //path to the SQLite database file
+        $db_file = './myDB/airport.db';
+
+        try {
+            //open connection to the airport database file
+            $db = new PDO('sqlite:' . $db_file);      // <------ Line 13
+
+            //set errormode to use exceptions
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            //return all passengers, and store the result set
+            $query_str = "select * from passengers;";  // <----- Line 19
+            $result_set = $db->query($query_str);
+
+            //loop through each tuple in result set and print out the data
+            //ssn will be shown in blue (see below)
+            foreach($result_set as $tuple) {          // <------ Line 24
+                echo "<font color='blue'>$tuple[ssn]</font> $tuple[f_name] $tuple[m_name] $tuple[l_name]<br/>\n";
+            }
+
+            //disconnect from db
+            $db = null;
+        }
+        catch(PDOException $e) {
+            die('Exception : '.$e->getMessage());
+        }
+    ?>
+
+</p>
+</body>
+</html>
