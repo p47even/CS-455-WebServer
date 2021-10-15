e28159dd7e79bebab5e945204f16f86dab2d0e68
Eli Arao <earao@hotmail.com>
2021-10-14 20:20:55 -0700

    Attempting to pass data from htmlForm to inputHandler

--- public_html/htmlForm.php
+++ public_html/htmlForm.php
@@ -4,15 +4,20 @@
     
 
     <?php
+        $old_ssn = $_GET[oldSsn];
+        $old_fname = $_GET[oldFName];
+        $old_mname = $_GET[oldMName];
+        $old_lname = $_GET[oldLName];
+
         echo
-        "<form action='inputHandler.php' method='post' value='$_GET[oldSsn]'>
-        SSN: <input type='text' name='$_GET[oldSsn]' id='var'><br>
-        <form action='inputHandler.php' method='post' value='$_GET[oldFName]'>
-        First Name: <input type='text' name='$_GET[oldFName]' id='fName'><br>
-        <form action='inputHandler.php' method='post' value='$_GET[oldMName]'>
-        Middle Name: <input type='text' name='$_GET[oldMName]' id='mName'><br>
-        <form action='inputHandler.php' method='post' value='$_GET[oldLName]'>
-        Last Name: <input type='text' name='$_GET[oldLName]' id='lName'><br>
+        "<form action='inputHandler.php' method='post'>
+        SSN: <input type='text' name='ssn' id='ssn'><br>
+        <form action='inputHandler.php' method='post'>
+        First Name: <input type='text' name='fName' id='fName'><br>
+        <form action='inputHandler.php' method='post'>
+        Middle Name: <input type='text' name='mName' id='mName'><br>
+        <form action='inputHandler.php' method='post'>
+        Last Name: <input type='text' name='lName' id='lName'><br>
         <input type='submit' name='submit' value='Update Info'>"
         ;
     ?>
