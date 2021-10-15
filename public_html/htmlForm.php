5c75c95c283e2457e28af886fc5f1a58056f13f3
Eli Arao <earao@hotmail.com>
2021-10-14 19:10:03 -0700

    generate forms now in php test

--- public_html/htmlForm.php
+++ public_html/htmlForm.php
@@ -4,12 +4,9 @@
     
 
     <?php
-        $oldName = $_GET[oldName];
-        $oldSsn = $_GET[oldSsn];
-
-        echo "<form action='ipnutHandler.php' name='form' method='post' value='$oldName'>
+        echo "<form action='ipnutHandler.php' name='form' method='post' value='$_GET[oldName]'>
         Attr: <input type='text' name='attr' id='attr'><br>
-        <form action='inputHandler.php' name='form' method='post' value='$oldSsn'>
+        <form action='inputHandler.php' name='form' method='post' value='$_GET[oldSsn]'>
         Var: <input type='text' name='var' id='var'><br>
         <input type='submit' name='submit' value='Update Info'>";
     ?>
