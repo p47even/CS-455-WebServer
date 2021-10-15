251077a469a856b76745852a7523c04483e43153
Eli Arao <earao@hotmail.com>
2021-10-14 17:56:13 -0700

    Modified showpassengers to once again dispaly all passengers, added htmlForm to get input and inputHandler to handle it. NOTICE: inputHandler.php does not yet work and has some placeholder values

--- public_html/showPassengers.php
+++ public_html/showPassengers.php
@@ -6,7 +6,7 @@
 Attr: <input type="text" name="attr" id="attr"><br>
 <form action="showPassengers.php" name='form' method='get'>
 Var: <input type="text" name="var" id="var"><br>
-<input type="submit" name="submit" value="Submit">
+<input type="submit" name="submit" value="Update Info">
 
 <h2>Query Results</h2>
 <p>
@@ -23,7 +23,7 @@ Var: <input type="text" name="var" id="var"><br>
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             //return the html query
-            $query_str = "select * from passengers where $_GET[attr]='$_GET[var]';"; // <------ Line 19
+            $query_str = "select * from passengers;"; // <------ Line 19
             $result_set = $db->query($query_str);
 
             //loop through each tuple in result set and print out the data
