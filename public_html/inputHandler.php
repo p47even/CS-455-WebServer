05a9a72fe5242517fa9bc7c7ced6df48f1cab232
Eli Arao <earao@hotmail.com>
2021-10-19 12:14:04 -0700

    Added html and redirecting to inputHandler

--- public_html/inputHandler.php
+++ public_html/inputHandler.php
@@ -1,56 +1,63 @@
-<?php
-session_start();
-//path to the SQLite database file
-$db_file = './myDB/airport.db';
-##require './htmlForm.php';
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+    session_start();
+    //path to the SQLite database file
+    $db_file = './myDB/airport.db';
+    ##require './htmlForm.php';
 
-////print_r($GLOBALS);
+    ////print_r($GLOBALS);
 
-try {
+    try {
 
-    //open connection to the airport database file
-    $db = new PDO('sqlite:' . $db_file);      // <------ Line 13
+        //open connection to the airport database file
+        $db = new PDO('sqlite:' . $db_file);      // <------ Line 13
 
-    //set errormode to use exceptions
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //set errormode to use exceptions
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    /*
-    $updateSSN = $db->prepare("UPDATE passengers SET (old_f_name = new_f_name, old_m_name = new_m_name, old_l_name = new_l_name, old_ssn = new_ssn) WHERE (ssn == old_ssn)");
-        $updateSSN->bindParam('new_f_name', $new_f_name);
-        $updateSSN->bindParam('new_m_name', $new_m_name);
-        $updateSSN->bindParam('new_l_name', $new_l_name);
-        $updateSSN->bindParam('new_ssn', $new_ssn);
-        $updateSSN->bindParam('old_f_name', $old_f_name);
-        $updateSSN->bindParam('old_m_name', $old_m_name);
-        $updateSSN->bindParam('old_l_name', $old_l_name);
-        $updateSSN->bindParam('old_ssn', $old_ssn);
-    */
-  
-    $new_f_name = $_POST["fName"];
-    $new_m_name = $_POST["mName"];
-    $new_l_name = $_POST["lName"];
-    $new_ssn = $_POST["ssn"];
-
-    $f_name = $_SESSION["oldFName"];
-    $m_name = $_SESSION["oldMName"];
-    $l_name = $_SESSION["oldLName"];
-    $old_ssn = $_SESSION["oldSsn"];
+        /*
+        $updateSSN = $db->prepare("UPDATE passengers SET (old_f_name = new_f_name, old_m_name = new_m_name, old_l_name = new_l_name, old_ssn = new_ssn) WHERE (ssn == old_ssn)");
+            $updateSSN->bindParam('new_f_name', $new_f_name);
+            $updateSSN->bindParam('new_m_name', $new_m_name);
+            $updateSSN->bindParam('new_l_name', $new_l_name);
+            $updateSSN->bindParam('new_ssn', $new_ssn);
+            $updateSSN->bindParam('old_f_name', $old_f_name);
+            $updateSSN->bindParam('old_m_name', $old_m_name);
+            $updateSSN->bindParam('old_l_name', $old_l_name);
+            $updateSSN->bindParam('old_ssn', $old_ssn);
+        */
     
-    $f_name_query = "UPDATE passengers SET f_name = '$new_f_name' WHERE ssn = $old_ssn;";
-    $m_name_query = "UPDATE passengers SET m_name = '$new_m_name' WHERE ssn = $old_ssn;";
-    $l_name_query = "UPDATE passengers SET l_name = '$new_l_name' WHERE ssn = $old_ssn;";
-    $ssn_query = "UPDATE passengers SET ssn = $new_ssn WHERE ssn = $old_ssn;";
+        $new_f_name = $_POST["fName"];
+        $new_m_name = $_POST["mName"];
+        $new_l_name = $_POST["lName"];
+        $new_ssn = $_POST["ssn"];
 
-    $db->query($f_name_query);
-    $db->query($m_name_query);
-    $db->query($l_name_query);
-    $db->query($ssn_query);
+        $f_name = $_SESSION["oldFName"];
+        $m_name = $_SESSION["oldMName"];
+        $l_name = $_SESSION["oldLName"];
+        $old_ssn = $_SESSION["oldSsn"];
+        
+        $f_name_query = "UPDATE passengers SET f_name = '$new_f_name' WHERE ssn = $old_ssn;";
+        $m_name_query = "UPDATE passengers SET m_name = '$new_m_name' WHERE ssn = $old_ssn;";
+        $l_name_query = "UPDATE passengers SET l_name = '$new_l_name' WHERE ssn = $old_ssn;";
+        $ssn_query = "UPDATE passengers SET ssn = $new_ssn WHERE ssn = $old_ssn;";
 
-    //disconnect from db
-    $db = null;
-}
-catch(PDOException $e) {
-    die('Exception : '.$e->getMessage());
-}
-session_destroy();
-?>
+        $db->query($f_name_query);
+        $db->query($m_name_query);
+        $db->query($l_name_query);
+        $db->query($ssn_query);
+
+        //disconnect from db
+        $db = null;
+    }
+    catch(PDOException $e) {
+        die('Exception : '.$e->getMessage());
+    }
+    session_destroy();
+    ?>
+    <meta http-equiv="refresh" content="0; url=./showPassengers.php"/>
+    <a href="./showPassengers.php">If you are not redirected, click here</a>
+</body>
+</html>
