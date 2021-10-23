d29e70c1a7cac12d999970ee2e0c2564b0979189
Eli Arao <earao@hotmail.com>
2021-10-22 17:09:31 -0700

    Added space to fail statements

--- public_html/inputHandler.php
+++ public_html/inputHandler.php
@@ -39,23 +39,23 @@
 
         if(!preg_match("/[a-zA-Z]*/", $new_f_name))
         {
-            $msg .= "First name must be non-empty and consist of letters only  ";
+            $msg .= "First name must be non-empty and consist of letters only  \n";
         }
 
 
         if(!preg_match("/[a-zA-Z]/", $new_m_name) && (strcmp("", $new_m_name) != 0))
         {
-            $msg .= "Middle name must be empty or a single letter  ";
+            $msg .= "Middle name must be empty or a single letter  \n";
         }
 
         if(!preg_match("/[a-zA-Z]*/", $new_l_name))
         {
-            $msg .= "Last name must be non-empty and consist of letters only  ";
+            $msg .= "Last name must be non-empty and consist of letters only  \n";
         }
         
         if(!preg_match("/[0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]/", $new_ssn))
         {
-            $msg .= "SSN must be in the form xxx-xx-xxxx where x is a whole number between 0 and 9  ";
+            $msg .= "SSN must be in the form xxx-xx-xxxx where x is a whole number between 0 and 9  \n";
         }
 
         if(strcmp("", $msg) == 0)
