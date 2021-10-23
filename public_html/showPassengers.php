b86e7d912195ce3110432b8efc28202888205744
Eli Arao <earao@hotmail.com>
2021-10-22 19:24:34 -0700

    Added add new ppassenger button to showPassengers and added adding code in inputHandler

--- public_html/showPassengers.php
+++ public_html/showPassengers.php
@@ -28,8 +28,8 @@
             foreach($result_set as $tuple) {          // <------ Line 24
                 echo "<font color='blue'>$tuple[ssn]</font> $tuple[f_name] $tuple[m_name] $tuple[l_name] \t<a href='./htmlForm.php?oldSsn=$tuple[ssn]&oldFName=$tuple[f_name]&oldMName=$tuple[m_name]&oldLName=$tuple[l_name]'> Update </a> <br/>\n";
             }
-            <br><br>
-            echo $_GET["msg"] .= "";
+            
+            echo "<br><br>" .= $_GET["msg"];
 
             //disconnect from db
             $db = null;
