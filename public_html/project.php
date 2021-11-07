3953f7a264824929846af7a7524244d4ee970b27
ericmarkewitz <61162496+ericmarkewitz@users.noreply.github.com>
2021-11-07 16:45:45 -0700

    Update project.php

--- public_html/project.php
+++ public_html/project.php
@@ -10,22 +10,36 @@
                         <div class='menu'>
                                 <h1>Enrollment Planner Login!</h1>
                         </div>
-                        <div class='sidebar'>Sidebar</div>
-                        <div class='content'>Content</div>
+                        <div class='sidebar'>
+                                <?php
+                                        echo
+                                        "<form action='./login/login.php' method='post'>
+                                        Username: <input type='text' name='username' id='username'><br>
+                                        <form action='./login/login.php' method='post'>
+                                        Password: <input type='text' name='password' id='password'><br>
+              
+                                        <input type='submit' name='submit' value='Student Login'>"
+                                        ;
+                                ?>
+                        </div>
+                        
+                        <div class='content'>
+                                <?php
+                                        echo
+                                        "<form action='./login/login.php' method='post'>
+                                        Username: <input type='text' name='username' id='username'><br>
+                                        <form action='./login/login.php' method='post'>
+                                        Password: <input type='text' name='password' id='password'><br>
+              
+                                        <input type='submit' name='submit' value='Admin Login'>"
+                                        ;
+                                ?>
+                        </div>
+                        
                         <div class='footer'>Footer</div>
                 </div>
                 
                 
-                <?php
-                echo
-                "<form action='./login/login.php' method='post'>
-                Username: <input type='text' name='username' id='username'><br>
-                <form action='./login/login.php' method='post'>
-                Password: <input type='text' name='password' id='password'><br>
-              
-                <input type='submit' name='submit' value='Student Login'>"
-                ;
                 
-                ?>
         </body>
 </html>
