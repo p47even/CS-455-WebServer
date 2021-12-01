2246222a9b73d6109890544c7cc7df2257056696
Eli Arao <earao@hotmail.com>
2021-12-01 15:28:19 -0800

    Made a page to get data from db and pass it via a session to be displayed

---
+++ public_html/searchClassesTemplate.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<body>
+        <form action='./searchClasses.php' method='post'>
+        courseID: <input type='text' name='courseID' id='courseID'><br>
+
+        <form action='./searchClasses.php' method='post'>
+        deptID: <input type='text' name='deptID' id='deptID'><br>
+
+        <form action='./searchClasses.php' method='post'>
+        courseName: <input type='text' name='courseName' id='courseName'><br>
+
+        <form action='./searchClasses.php' method='post'>
+        fallSemester: <input type='text' name='fallSemester' id='fallSemester'><br>
+        
+        <form action='./searchClasses.php' method='post'>
+        springSemester: <input type='text' name='springSemester' id='springSemester'><br>
+        
+        <input type='submit' name='submit' value='Update Info'>
+    <br>
+    <br>
+
+    <?php>
+        foreach($_SESSION["courAttrQuer"] as $tuple) {
+                echo "<font color='blue'>$tuple[courseID]</font> $tuple[deptID] $tuple[courseName] $tuple[fallSemester] $tuple[springSemester]\n";
+            }
+    ?>
+</body>
+</html>
