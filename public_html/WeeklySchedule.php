49bd58335eca26a97843f6b598e57aa998761896
svazquezj <61162725+svazquezj@users.noreply.github.com>
2021-12-02 09:53:35 -0800

    Update WeeklySchedule.php
    Added a table to display results

--- public_html/WeeklySchedule.php
+++ public_html/WeeklySchedule.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+<head>
 <style>
         .toolbar{
             background-color: maroon;
@@ -18,25 +19,14 @@
     </style>
     <div class="toolbar">
         <a href="Dashboard.html">Home</a>
-        <a href="WeeklySchedule.html">Schedule</a>
+        <a href="WeeklySchedule.php">Schedule</a>
         <a href="Search4Classes">Search for classes</a>
         <a href="AcademicRequirements">Academic Rqueirements</a>
         <a href="Enrollment.php">Enroll</a>
-        <a href="4YearPlanSample.php">Four Year Plan</a>
+        <a href="4YearPlan.php">Four Year Plan</a>
     </div>
 
-    <table>
-        <tr>
-            <th>Select</th>
-            <th>Class #</th>
-            <th>Class Tittle</th>
-            <th>Instructor</th>
-            <th>Meeting Time</th>
-            <th>Meeting Days</th>
-        </tr>
-
-<h2>Weekly Scedule</h2>
-<body>
+    <h2>Weekly Scedule</h2>
     <?php
 
     try {
@@ -47,22 +37,83 @@
         //set errormode to use exceptions
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
-        $query_str = $db->query("SELECT * FROM Enroll NATURAL JOIN IsMeeting NATURAL JOIN Course WHERE studentID = 1 GROUP BY courseID;");
-         
-        $classes = "";
-        foreach($query_str as $class){
-            $classes = array(array("$class[meetDay] $class[meetTime] $class[endTime] $class[courseName] $class[location]\t"));
+        $query_str = $db->query("SELECT * FROM Enroll NATURAL JOIN IsMeeting NATURAL JOIN Course WHERE studentID = 1;");
+        $monClass = $db->query("SELECT meetTime, endTime, courseName, location FROM $query_str WHERE meetDay = 'Monday' ORDER BY meetTime;");
+        $tuClass = $db->query("SELECT meetTime, endTime, courseName, location FROM $query_str WHERE meetDay = 'Tuesday' ORDER BY meetTime;");
+        $wedClass = $db->query("SELECT meetTime, endTime, courseName, location FROM $query_str WHERE meetDay = 'Wedmesday' ORDER BY meetTime;");
+        $thClass = $db->query("SELECT  meetTime, endTime, courseName, location FROM $query_str WHERE meetDay = 'Thursday' ORDER BY meetTime;"); 
+        $frClass = $db->query("SELECT meetTime, endTime, courseName, location FROM $query_str WHERE meetDay = 'Friday' ORDER BY meetTime;");
+        
+        $monClassesArray = array();
+        $tuClassesArray = array();
+        $wedClassesArray = array();
+        $thClassesArray = array();
+        $frClassesArray = array();
+
+        foreach($monClasss as $class){
+            $monClassesArray = array("$class[meetTime] $class[endTime] $class[courseName] $class[location]\t");
+        }
+
+        foreach($tuClasss as $class){
+            $tuClassesArray = array("$class[meetTime] $class[endTime] $class[courseName] $class[location]\t");
+        }
+
+        foreach($wedClasss as $class){
+            $wedClassesArray = array("$class[meetTime] $class[endTime] $class[courseName] $class[location]\t");
+        }
+
+        foreach($thClasss as $class){
+            $thClassesArray = array("$class[meetTime] $class[endTime] $class[courseName] $class[location]\t");
+        }
+
+        foreach($frClasss as $class){
+            $frClassesArray = array("$class[meetTime] $class[endTime] $class[courseName] $class[location]\t");
         }
       
         $db = null;
 
-        foreach($classes as $class){
-            echo "<tr>$class</tr>";
-        }
     }
     catch(PDOException $e) {
         die('Exception : '.$e->getMessage());
     }
     ?>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>Sunday</th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturday</th>
+        </tr>
+        
+        <tr>
+            <th><th>
+            <th><?php foreach ($monClassesArray as $class){
+                echo $class;
+            } ?></th>
+            <th><?php foreach ($tuClassesArray as $class){
+                echo $class;
+            } ?></th>
+
+            <th><?php foreach ($wedClassesArray as $class){
+                echo $class;
+            } ?></th>
+
+            <th><?php foreach ($thClassesArray as $class){
+                echo $class;
+            } ?></th>
+
+            <th><?php foreach ($frClassesArray as $class){
+                echo $class;
+            } ?></th>
+
+            <th></th>
+        </tr>
+
+    </table>
 </body>
 </html>
