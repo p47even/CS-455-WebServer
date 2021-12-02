f36cb5b42b7508fba5a52d5bdeaf03ff69e487ea
svazquezj <61162725+svazquezj@users.noreply.github.com>
2021-12-01 17:49:15 -0800

    Update WeeklySchedule.php
    Added some styling and fixed other things

--- public_html/WeeklySchedule.php
+++ public_html/WeeklySchedule.php
@@ -5,14 +5,18 @@
 
     try {
 
-        //open connection to the airport database file
+        //open connection to the university's database file
         $db = new PDO('sqlite:' . './uni.db');      // <------ Line 13
 
         //set errormode to use exceptions
-        //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
-        $classes = $db->query("SELECT * FROM Enroll NATURAL JOIN IsMeeting WHERE studentID = 1;");
-         echo $classes;
+        $query_str = $db->query("SELECT * FROM Enroll NATURAL JOIN IsMeeting WHERE studentID = 1;");
+        $classes = $db->query($query_str);
+         
+        foreach($classes as $class){
+            echo "$class[meetTime] $class[meetDay] $class[className] $class[location]\t";
+        }
       
         $db = null;
     }
