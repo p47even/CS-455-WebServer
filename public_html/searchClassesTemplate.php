b40e554e0fb17d8e5168838313941861d23c9195
Eli Arao <earao@hotmail.com>
2021-12-04 14:15:49 -0800

    Trying to test out different ways to submit form data without redirection

--- public_html/searchClassesTemplate.php
+++ public_html/searchClassesTemplate.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html>
 <body>
-        <form action='./searchClasses.php' method='post'>
+<iframe name="dummyframe" id="dummyframe" style="display: none;"></iframe>
+
+        <form action='./searchClasses.php' target="dummyframe" method='post'>
         courseID: <input type='text' name='courseID' id='courseID'><br>
 
-        <form action='./searchClasses.php' method='post'>
+        <form action='./searchClasses.php' target="dummyframe" method='post'>
         deptID: <input type='text' name='deptID' id='deptID'><br>
 
-        <form action='./searchClasses.php' method='post'>
+        <form action='./searchClasses.php' target="dummyframe" method='post'>
         courseName: <input type='text' name='courseName' id='courseName'><br>
 
-        <form action='./searchClasses.php' method='post'>
+        <form action='./searchClasses.php' target="dummyframe" method='post'>
         fallSemester: <input type='text' name='fallSemester' id='fallSemester'><br>
         
-        <form action='./searchClasses.php' method='post'>
+        <form action='./searchClasses.php' target="dummyframe" method='post'>
         springSemester: <input type='text' name='springSemester' id='springSemester'><br>
         
         <input type='submit' name='submit' value='Update Info'>
