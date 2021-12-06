40277c794d501f97dce10d4758db95a253f289f6
ericmarkewitz <61162496+ericmarkewitz@users.noreply.github.com>
2021-12-06 02:14:29 -0800

    Hashing passwords

--- public_html/phpTests.php
+++ public_html/phpTests.php
@@ -4,7 +4,7 @@ $userPassPairs = array("0"=>"zSLPj4JV","1"=>"2JxCQnub","2"=>"4vmZmHA3","3"=>"5tL
 foreach($userPassPairs as $id => $pass){
 
     $hashedPass = hash('sha256', $pass, false);
-    echo "UPDATE StudentLogIn SET password = " . $hashedPass ." WHERE StudentID = ". $id."<br>";
+    echo "UPDATE StudentLogin SET stuPassword = " . $hashedPass ." WHERE studentID = ". $id."<br>";
 
 }
 ?>
