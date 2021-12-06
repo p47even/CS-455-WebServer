27b86994089247ba345b40862dff7e4d7d9f76eb
ericmarkewitz <61162496+ericmarkewitz@users.noreply.github.com>
2021-12-05 22:38:48 -0800

    figuring out form handling

--- public_html/project.php
+++ public_html/project.php
@@ -10,11 +10,14 @@
                 <div class='main'>
                     <div class='studentLogin'>
                         <h2>Student Login!</h2>
-                        <form action='./login/login.php' method='post'>
-                        StudentID: <input type='text' name='username' id='username'><br>
-                        <form action='./login/login.php' method='post'>
-                        Password: <input type='password' name='password' id='password'><br>
-                        <input type='submit' name='submit' value='Login'>
+                        <?php
+                            echo "
+                            <form action='./login/login.php' method='post'>
+                            StudentID: <input type='text' name='username' id='username'><br>
+                            <form action='./login/login.php' method='post'>
+                            Password: <input type='password' name='password' id='password'><br>
+                            <input type='submit' name='submit' value='Login'>";
+                        ?>
                     </div>
 
                     <div class='adminLogin'>
