a4ab28cb744d97b3094b3efe8493c46d49049567
ericmarkewitz <61162496+ericmarkewitz@users.noreply.github.com>
2021-12-05 23:36:42 -0800

    figuring out form handling - messing with echo statements

--- public_html/project.php
+++ public_html/project.php
@@ -10,29 +10,29 @@
                 <div class='main'>
                     <div class='studentLogin'>
                         <h2>Student Login!</h2>
-                        <?php
-                            echo
-                            "<form action='./login/login.php' method='post'>
-                            StudentID: <input type='text' name='username' id='username'><br>
 
-                            <form action='./login/login.php' method='post'>
-                            Password: <input type='password' name='password' id='password'><br>
+                        <form action='./login/login.php' method='post'>
+                        StudentID: <input type='text' name='username' id='username'><br>
 
-                            <input type='submit' name='submit' value='Login'>"
-                            ;
-                        ?>
+                        <form action='./login/login.php' method='post'>
+                        Password: <input type='password' name='password' id='password'><br>
+
+                        <input type='submit' name='submit' value='Login'>
+
                     </div>
 
                     <div class='adminLogin'>
                         <h2>Admin Login!</h2>
+
                         <form action='./login/adminLogin.php/' method='post'>
-                        AdminID: <input type='text' name='username' id='username'><br>
+                        AdminID: <input type='text' name='admin_username' id='admin_username'><br>
+
                         <form action='./login/adminLogin.php' method='post'>
-                        Password: <input type='text' name='password' id='password'><br>
+                        Password: <input type='password' name='admin_password' id='admin_password'><br>
                         <input type='submit' name='submit' value='Login'>
                     </div>
                 </div>
-                <!--
+
                 <?php
                     $error_message = $_GET["msg"];
 
@@ -41,6 +41,5 @@
 
                     }
                 ?>
-                -->
         </body>
 </html>
