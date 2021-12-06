1ac60f355bc9acba29bf9cf3925c114feb674eb2
Justus Bayat <61127228+p47even@users.noreply.github.com>
2021-12-06 13:58:19 -0800

    fixed links pt 2

--- public_html/4YearPlan.php
+++ public_html/4YearPlan.php
@@ -259,8 +259,8 @@
         </style>
         <div class="toolbar">
         <a href="Dashboard.php">Home</a>
-        <a href="WeeklySchedule.php">Schedule</a>
-        <a href="ProfSchedule.php">Schedule</a>
+        <a href="WeeklySchedule.php">Weekly Schedule</a>
+        <a href="ProfSchedule.php">Prof Schedule</a>
         <a href="searchClasses.php">Search for classes</a>
         <a href="ClassRoster.php">Class Roster</a>
         <a href="AddClassForm.php">Add Class</a>
