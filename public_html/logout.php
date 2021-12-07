ee6bd0ce1a621ee0234c8d9893023d5ea5b0a07c
Eli Arao <earao@hotmail.com>
2021-12-07 14:07:47 -0800

    added logout.php to un-assign session vars and return to login

--- public_html/logout.php
+++ public_html/logout.php
@@ -3,17 +3,17 @@
     
     if(isset($_SESSION["sID"]))
     {
-        usnet($_SESSION["sID"]);
+        unset($_SESSION["sID"]);
     }
 
     if(isset($_SESSION["fID"]))
     {
-        usnet($_SESSION["fID"]);
+        unset($_SESSION["fID"]);
     }
 
     if(isset($_SESSION["cart"]))
     {
-        usnet($_SESSION["cart"]);
+        unset($_SESSION["cart"]);
     }
 
     $redirect_url = "./project.php";
