5b797fc2d37ca2849ed0afbac5cc80bcfe66db58
svazquezj <61162725+svazquezj@users.noreply.github.com>
2021-12-08 09:56:45 -0800

    Testing

--- public_html/removeClassHandler.php
+++ public_html/removeClassHandler.php
@@ -70,7 +70,7 @@
                 $class_deleted = $delete_class->execute();
                 echo "<meta http-equiv='refresh' content='0; url=./removeClass.php'/>";
             } else {
-                
+                echo "<meta http-equiv='refresh' content='0; url=./removeClass.php'/>";
             }
         }
         catch(PDOException $e) {
