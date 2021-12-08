34d871f6f6521e4f988bfdafd8964bd218621f93
ericmarkewitz <61162496+ericmarkewitz@users.noreply.github.com>
2021-12-07 22:07:03 -0800

    Working on addStudentHandler

---
+++ public_html/addStudentHandler.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+                .toolbar{
+                    background-color: maroon;
+                }
+                .toolbar a:hover{
+                    background-color:white;
+                    color: black
+                }
+
+                .toolbar a{
+                    padding: 15px 15px;
+                    color: white;
+                    font-size: 20px;
+                    text-decoration: none;
+                }
+                table, th, td{
+                    border: 1px solid black;
+                    border-collapse: collapse;
+                }
+
+                table.center {
+                    margin-left: auto;
+                    margin-right: auto;
+                }
+
+                th{
+                    color: maroon;
+                }
+                h2{
+                    text-align: center;
+                }
+            </style>
+            <!--<div class="toolbar">
+                <a href="dashboard.php">Home</a>
+                <a href="WeeklySchedule.php">Schedule</a>
+                <a href="searchClassesTemplate.php">Search for Classes</a>
+                <a href="AcademicRequirements">Academic Requirements</a>
+                <a href="Enrollment.php?msg=">Enroll</a>
+                <a href="Discussion.html">Discussion Board</a>
+                <a href="4YearPlan.php">Four Year Plan</a>
+                <a href="logout.php" class="logout">Logout</a>
+            </div> -->
+
+    </head>
+    <body>
+            <h1>Add a New Student</h1>
+
+            <form action='./addStudent.php' method='post'>
+            Student Name: <input type='text' name='studentName' id='studentName'><br>
+
+            <form action='./addStudent.php' method='post'>
+            StudentID: <input type='text' name='studentID' id='studentID'><br>
+
+            <form action='./addStudent.php' method='post'>
+            Class: <select name='class' id='class'>
+                      <option value='1'>Freshman</option>
+                      <option value='2'>Sophmore</option>
+                      <option value='3'>Junior</option>
+                      <option value='4'>Senior</option>
+                      <option value='5'>Other</option>
+
+            <form action='./addStudent.php' method='post'>
+            GPA: <input type='text' name='gpa' id='gpa'><br>
+
+            <form action='./addStudent.php' method='post'>
+            Major: <select name='major' id='major'>
+                      <option value='undecided'>Undecided</option>
+                      <option value='bio'>Biology</option>
+                      <option value='chem'>Chemistry</option>
+                      <option value='cs'>Computer Science</option>
+                      <option value='eng'>English</option>
+                      <option value='hist'>History</option>
+                      <option value='math'>Mathematics</option>
+
+            <form action='./addStudent.php' method='post'>
+            username: <input type='text' name='username' id='username'><br>
+
+            <form action='./addStudent.php' method='post'>
+            password: <input type='password' name='password' id='password'><br>
+
+            <input type='submit' name='submit' value='Create'>
+        <br>
+        <br>
+
+        <p>
+            <?php
+                session_start();
+
+                if(!isset($_SESSION["fID"]))
+                {
+                     header("Location: 'fProject.php?msg=Please Login First'", true, 303);
+                     exit;
+                }
+
+            ?>
+        </p>
+
+    </body>
+</html>
