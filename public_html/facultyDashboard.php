c023a494c5a3ae63f6b727bc739dee38483c05cc
svazquezj <61162725+svazquezj@users.noreply.github.com>
2021-12-07 20:41:46 -0800

    Update facultyDashboard.php

--- public_html/facultyDashboard.php
+++ public_html/facultyDashboard.php
@@ -38,12 +38,23 @@
     </style>
     <div class="toolbar">
     <a href="facultyDashboard.php">Home</a>
+<<<<<<< HEAD
         <!--<a href="ProfSchedule.php">Schedule</a> -->
         <!--<a href="searchClasses.php">Search for classes</a> -->
         <!--<a href="ClassRoster.php">Class Roster</a> -->
         <!--<a href="AddClassPromp.php">Add Class</a> -->
        <!-- <a href="removeClass.php">Remove Class</a> -->
         <!-- <a href="logout.php" class="logout">Logout</a> -->
+=======
+        <!-- <a href="WeeklySchedule.php">Schedule</a> -->
+        <!-- <a href="searchClassesTemplate.php">Search for Classes</a> -->
+        <!-- <a href="AcademicRequirements">Academic Requirements</a> -->
+        <!-- <a href="Enrollment.php?msg=">Enroll</a> -->
+        <!-- <a href="Discussion.html">Discussion Board</a> -->
+        <!-- <a href="4YearPlan.php">Four Year Plan</a> -->
+        <a href="addStudent.php">Add New Student</a>
+        <a href="logout.php" class="logout">Logout</a>
+>>>>>>> 1c98b7057d00881b4d93e596dbd7aa180df73d09
     </div>
         </head>
         <body>
