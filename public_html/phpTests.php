5d7f7fddaae71a8c2ff69c621e23179dccac9610
ericmarkewitz <61162496+ericmarkewitz@users.noreply.github.com>
2021-12-07 19:57:39 -0800

    Troubleshooting php tests

--- public_html/phpTests.php
+++ public_html/phpTests.php
@@ -6,11 +6,11 @@ $facPassPairs = array("0"=>"dc4I}+sDY","1"=>"7N~.}MgKfX","2"=>"m0]!lFjvpp","3"=>
 foreach($userPassPairs as $id => $pass){
 
     $hashedPass = hash('sha256', $pass, false);
-    echo "UPDATE StudentLogIn SET password = " . $hashedPass . " WHERE StudentID = " . $id;
+    echo "UPDATE StudentLogIn SET password = " . $hashedPass . " WHERE StudentID = " . $id . "<br>";
 }
 echo "<br>";
 foreach($facPassPairs as $id => $pass){
     $hashedPass = hash('sha256', $pass, false);
-    echo "UPDATE FacultyLogin SET password = " . $hashedPass ." WHERE FacultyID = ". $id;
+    echo "UPDATE FacultyLogin SET password = " . $hashedPass ." WHERE FacultyID = ". $id . "<br>";
 }
 ?>
