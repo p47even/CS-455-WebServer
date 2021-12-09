f883327cc382e2c01ba027bae30f8a18b9f896e3
svazquezj <61162725+svazquezj@users.noreply.github.com>
2021-12-09 11:47:47 -0800

    Update ProfSchedule.php

--- public_html/ProfSchedule.php
+++ public_html/ProfSchedule.php
@@ -34,6 +34,7 @@
             text-align: center;
         }
     </style>
+    <!-- Tool bar that helps users navigate between pages -->
     <div class="toolbar">
         <a href="facultyDashboard.php">Home</a>
         <a href="ProfSchedule.php">Schedule</a>
@@ -47,13 +48,15 @@
     <?php
     session_start();
     try {
-
+        //Checks to see if a faculty member is logged in. 
         if(!isset($_SESSION["fID"]))
         { 
+            //faculty member is not logged in so redirect to log in page
             $loginUrl = 'project.php?msg=Please Login First';
             header("Location: $loginUrl", true, 303);
             exit; 
         }
+        //gets the ID of user
         $faculty_ID = $_SESSION["fID"];
         //open connection to the university's database file
         $db = new PDO('sqlite:' . './myDB/uni.db');      // <------ Line 13
@@ -61,12 +64,14 @@
         //set errormode to use exceptions
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+        //variables that will keep track of classes taught by current user on a specific day of the week
         $monClass = "";
         $tuClass = "";
         $wedClass = "";
         $thClass = "";
         $frClass = "";
         
+        //gets classes taught by current user ordered chronologically
         $monClass = $db->query("SELECT meetTime, endTime, courseName, location FROM (SELECT * FROM Teaching NATURAL JOIN isMeeting NATURAL JOIN Course WHERE facultyID = $faculty_ID) WHERE meetDay = 'Monday' ORDER BY meetTime;");
         $tuClass = $db->query("SELECT meetTime, endTime, courseName, location FROM (SELECT * FROM Teaching NATURAL JOIN IsMeeting NATURAL JOIN Course WHERE facultyID = $faculty_ID) WHERE meetDay = 'Tuesday' ORDER BY meetTime;");
         $wedClass = $db->query("SELECT meetTime, endTime, courseName, location FROM (SELECT * FROM Teaching NATURAL JOIN IsMeeting NATURAL JOIN Course WHERE facultyID = $faculty_ID) WHERE meetDay = 'Wedmesday' ORDER BY meetTime;");
@@ -81,6 +86,9 @@
 </head>
 <body>
     <?php 
+            //Prints the schedule of the current user
+
+            //print headings of table that will hold user's schedule
             echo
                         "<table class='center'>
                             <tr>
@@ -89,7 +97,7 @@
                                 <th>Class</th>
                                 <th>Location</th>
                             </tr>"; 
-
+                //prints classes taught on mondays
                 echo "<tr><th>Monday</th><td></td><td></td><td></td></tr>";
                     foreach($monClass as $class) {
                         echo " <tr>
@@ -100,6 +108,7 @@
                                 </tr>"; 
                     }
 
+                //prints classes taught on Tuesday
                 echo "<tr><th>Tuesday</th><td></td><td></td><td></td></tr>";
                     foreach($tuClass as $class) {
                         echo " <tr>
@@ -110,6 +119,7 @@
                                 </tr>"; 
                     }
 
+                //prints classes taught on Wednesday
                 echo "<tr><th>Wednesday</th><td></td><td></td><td></td></tr>";
                     foreach($wedClass as $class) {
                         echo " <tr>
@@ -120,6 +130,7 @@
                                 </tr>"; 
                     }
 
+                //prints classes taught on Thursday
                 echo "<tr><th>Tursday</th><td></td><td></td><td></td></tr>";
                     foreach($thClass as $class) {
                         echo " <tr>
@@ -130,6 +141,7 @@
                                 </tr>"; 
                     }
 
+                //prints classes taught on Friday
                 echo "<tr><th>Friday</th><td></td><td></td><td>";
                     foreach($frClass as $class) {
                         echo "<tr>
