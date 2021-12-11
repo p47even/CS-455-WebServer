cb2a4a7dc56d54966f4acf80bc3cfbc2d02ca2c2
Justus Bayat <61127228+p47even@users.noreply.github.com>
2021-12-10 21:24:09 -0800

    testing redirect change

--- public_html/searchCourses.php
+++ public_html/searchCourses.php
@@ -8,10 +8,11 @@
             { 
                 $loginUrl = 'project.php?msg=Please Login First';
                 header("Location: $loginUrl", true, 303);
+                //header("Location: $loginUrl", true, 303);
                 exit; 
             }
             
-            $_SESSION['redirect_url'] = $_SERVER['PHP_SELF'];
+            //$_SESSION['redirect_url'] = $_SERVER['PHP_SELF'];
 
 
     //path to the SQLite database file
